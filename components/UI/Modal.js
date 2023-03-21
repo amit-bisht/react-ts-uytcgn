23d717816a6111e688b3dc775efa8dc206dea348
amit-bisht <71182763+amit-bisht@users.noreply.github.com>
2023-03-21 10:05:01 +0530

    1 file added

---
+++ components/UI/Modal.js
@@ -0,0 +1,13 @@
+import Card from './Card';
+import Button from './Button';
+function Modal() {
+  return (
+    <Card>
+      <header></header>
+      <div></div>
+      <footer>
+        <Button title="Save" type="button" />
+      </footer>
+    </Card>
+  );
+}
